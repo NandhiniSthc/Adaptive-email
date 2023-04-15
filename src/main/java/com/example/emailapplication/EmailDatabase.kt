709c24e9d32f0eaaff943ebf542b287c6f1b9414
NandhiniSthc <128118082+NandhiniSthc@users.noreply.github.com>
2023-04-15 11:42:57 +0530

    Add files via upload

---
+++ src/main/java/com/example/emailapplication/EmailDatabase.kt
@@ -0,0 +1,31 @@
+package com.example.emailapplication
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.emailapplication.EmailDao
+
+@Database(entities = [Email::class], version = 1)
+abstract class EmailDatabase : RoomDatabase() {
+
+    abstract fun emailDao(): EmailDao
+
+    companion object {
+
+        @Volatile
+        private var instance: EmailDatabase? = null
+
+        fun getDatabase(context: Context): EmailDatabase {
+            return instance ?: synchronized(this) {
+                val newInstance = Room.databaseBuilder(
+                    context.applicationContext,
+                    EmailDatabase::class.java,
+                    "email_database"
+                ).build()
+                instance = newInstance
+                newInstance
+            }
+        }
+    }
+}
